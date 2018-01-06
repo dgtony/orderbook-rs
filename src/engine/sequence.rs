9e091a26399874fb65140c6ecc20c513a8e7a012
Anton Dort-Golts <dortgolts@gmail.com>
2018-01-06 19:27:23 +0300

    Fix: sequence generator starts from min value

--- src/engine/sequence.rs
+++ src/engine/sequence.rs
@@ -3,20 +3,22 @@
 pub struct TradeSequence {
     min_id: u64,
     max_id: u64,
-    last_idx: u64,
+    current_idx: u64,
 }
 
 
 impl TradeSequence {
     pub fn next_id(&mut self) -> u64 {
-        let new_idx = if self.last_idx < self.max_id {
-            self.last_idx + 1
-        } else {
-            self.min_id
-        };
+        let next_id = self.current_idx;
 
-        self.last_idx = new_idx;
-        new_idx
+        // update index
+        if (next_id + 1) <= self.max_id {
+            self.current_idx += 1;
+        } else {
+            self.current_idx = self.min_id;
+        }
+
+        next_id
     }
 }
 
@@ -25,7 +27,7 @@ pub fn new_sequence_gen(min: u64, max: u64) -> TradeSequence {
     TradeSequence {
         min_id: min,
         max_id: max,
-        last_idx: min,
+        current_idx: min,
     }
 }
 
@@ -38,7 +40,8 @@ mod tests {
     fn seq_from_zero() {
         let mut seq_gen = new_sequence_gen(0, 2);
 
-        assert_eq!(seq_gen.last_idx, 0);
+        assert_eq!(seq_gen.current_idx, 0);
+        assert_eq!(seq_gen.next_id(), 0);
         assert_eq!(seq_gen.next_id(), 1);
         assert_eq!(seq_gen.next_id(), 2);
         assert_eq!(seq_gen.next_id(), 0);
@@ -48,9 +51,9 @@ mod tests {
     fn seq_from_positive() {
         let mut seq_gen = new_sequence_gen(1, 2);
 
-        assert_eq!(seq_gen.last_idx, 1);
-        assert_eq!(seq_gen.next_id(), 2);
+        assert_eq!(seq_gen.current_idx, 1);
         assert_eq!(seq_gen.next_id(), 1);
         assert_eq!(seq_gen.next_id(), 2);
+        assert_eq!(seq_gen.next_id(), 1);
     }
 }
