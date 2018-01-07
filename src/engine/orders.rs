20b70d0d2500097b92fda96ad003242368116103
Anton Dort-Golts <dortgolts@gmail.com>
2018-01-07 18:00:35 +0300

    Add: order request constructors

--- src/engine/orders.rs
+++ src/engine/orders.rs
@@ -1,14 +1,6 @@
 
-//use super::assets;
-//use super::assets::TradePair;
-
 use std::time::SystemTime;
-
-use super::domain;
 use super::domain::{OrderSide, Asset};
-
-
-
 
 
 pub enum OrderRequest {
@@ -37,53 +29,81 @@ pub enum OrderRequest {
     CancelOrder {
         id: u64,
         side: OrderSide,
-        ts: SystemTime,
+        //ts: SystemTime,
     },
 }
 
 
-pub enum RequestError<'a> {
-    UnsupportedAsset(&'a str),
-
-    //todo moar errors!
-}
-
 /* Constructors */
 
-// FIXME simplify signature?
-pub fn new_market_order_request<'a>(
-    order_asset: &'a str,
-    price_asset: &'a str,
+
+/// Create request for the new market order
+pub fn new_market_order_request(
+    order_asset: Asset,
+    price_asset: Asset,
     side: OrderSide,
     qty: f64,
     ts: SystemTime,
-) -> Result<OrderRequest, RequestError<'a>> {
+) -> OrderRequest {
 
-    // some validation
-    let order_asset = domain::parse_asset(order_asset).ok_or(
-        RequestError::UnsupportedAsset(order_asset),
-    )?;
-    let price_asset = domain::parse_asset(price_asset).ok_or(
-        RequestError::UnsupportedAsset(price_asset),
-    )?;
-
-    Ok(OrderRequest::NewMarketOrder {
+    OrderRequest::NewMarketOrder {
         order_asset,
         price_asset,
         qty,
         side,
         ts,
-    })
+    }
 }
 
 
-// TODO two more constructors!
+/// Create request for the new limit order
+pub fn new_limit_order_request(
+    order_asset: Asset,
+    price_asset: Asset,
+    side: OrderSide,
+    price: f64,
+    qty: f64,
+    ts: SystemTime,
+) -> OrderRequest {
+
+    OrderRequest::NewLimitOrder {
+        order_asset,
+        price_asset,
+        side,
+        price,
+        qty,
+        ts,
+    }
+}
 
 
-pub fn limit_order_cancel_request(order_id: u64, side: OrderSide, ts: SystemTime) -> OrderRequest {
+/// Create request for changing price/qty for the active limit order.
+///
+/// Note: do not change order side!
+/// Instead cancel existing order and create a new one.
+pub fn amend_order_request(
+    id: u64,
+    side: OrderSide,
+    price: f64,
+    qty: f64,
+    ts: SystemTime,
+) -> OrderRequest {
+
+    OrderRequest::AmendOrder {
+        id,
+        side,
+        price,
+        qty,
+        ts,
+    }
+}
+
+
+/// Create request for cancelling active limit order
+pub fn limit_order_cancel_request(order_id: u64, side: OrderSide) -> OrderRequest {
     OrderRequest::CancelOrder {
         id: order_id,
         side,
-        ts,
+        //ts,
     }
 }
